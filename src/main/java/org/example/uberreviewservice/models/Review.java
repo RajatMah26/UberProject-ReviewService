0832ba3b27ccc93fb1117332b05d5fd4b77bf75d
Rajatdhoot26 <2rajatd6@gmail.com>
2025-01-14 03:05:17 +0530

    Added first basic model

---
+++ src/main/java/org/example/uberreviewservice/models/Review.java
@@ -0,0 +1,13 @@
+package org.example.uberreviewservice.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id
+    Long id;
+
+}
